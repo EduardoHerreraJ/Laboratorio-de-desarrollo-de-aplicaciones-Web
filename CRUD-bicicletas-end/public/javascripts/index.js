83ae41ca6a449521f0b75d4bc05844bf95b85dd8
Eduardo Herrera <33185685+EduardoHerreraJ@users.noreply.github.com>
2022-03-23 23:57:29 -0600

    CRUD-bicicletas-end

---
+++ CRUD-bicicletas-end/public/javascripts/index.js
@@ -0,0 +1,9 @@
+// Importa la configuración de la app
+let appConfig = require('./configs/app');
+
+// Comienza el servidor en el puerto 3000 de localhost
+// para ver el sistema
+app.listen(appConfig.express_port,() => {
+  let show = 'App listening on port ' + appConfig.express_port + '! (http://localhost:' + appConfig.express_port +')';
+  console.log(show);
+});
